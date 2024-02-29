1b560279f51295241d1342cda49f68191869c8d0
Sawanjkotian <sawan9kotian@gmail.com>
2024-02-29 12:38:46 +0530

    GraphQL basics

---
+++ schema.js
@@ -0,0 +1,51 @@
+export const typeDefs = `#graphql
+type Game{
+    id: ID!
+    rating:Int!
+    title: String!
+    platform:[String!]!
+    reviews:[Review!]
+    author: Author!
+}
+
+type Review{
+    id:ID!
+    rating:Int!
+    content:String!
+    game: Game!
+    author: Author!
+}
+
+type Author{
+    id: ID!
+    name: String!
+    verified:Boolean!
+    reviews:[Review!]
+}
+
+
+type Query{
+    reviews:[Review]
+    review(id: ID): Review
+    games:[Game]
+    game(id: ID): Game
+    authors:[Author]
+    author(id:ID):Author
+}
+
+type Mutation{
+    addGame(game:AddGameInput!):Game
+    deleteGame(id: ID!): [Game]
+    updateGame(id: ID,  edits:EditGameInput!): Game
+}
+
+input AddGameInput{
+    title:String!,
+    platform:[String!]!
+}
+
+input EditGameInput{
+    title: String,
+    platform:[String!]
+}
+`
